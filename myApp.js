e36c3e9b610b5d40eac73e04ad38a377cfcea053
mrpickles-dev <mr.pickles300@gmail.com>
2021-05-05 23:42:55 +0200

    update

--- myApp.js
+++ myApp.js
@@ -1,17 +1,44 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
+console.log("Hello World");
+console.log(__dirname);
+
+app.use((req, response, next) => {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+});
+
+app.get("/", (request, response) => {
+  response.send("Hello Express");
+});
+
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/views/index.html");
+});
+
+app.use(express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public"));
+
+let message = { message: "Hello json" };
+
+app.get("/json", (request, response) => {
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    response.json({ message: "HELLO JSON" });
+  } else {
+    response.json(message);
+  }
+});
+
+app.get('/now', (request, response, next) => {
+  request.time = new Date().toString()
+  next()
+} , (request, response) => {
+  response.json({'time' : request.time})
+})
 
 
-
-
-
-
-
-
-
-
-
+module.exports = app;
 
 
 
